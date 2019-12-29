c155d1f01a93d836344e76841987ba165bbdc376
Keereslame <34709863+Keereslame@users.noreply.github.com>
2019-12-29 10:32:05 +0100

    Add files

--- Code/main_Version_Behrooz.py
+++ Code/main_Version_Behrooz.py
@@ -7,6 +7,7 @@ import RPi.GPIO as GPIO
 #setup pins for buttons, define states and events
 button_Start = 38
 button_Stop =  40
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(button_Start, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(button_Stop, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
