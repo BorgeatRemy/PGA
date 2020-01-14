0a700eed72179af12dd8bad879d8dfdf443c13c9
BorgeatRemy <remy.borgeat@students.hevs.ch>
2020-01-14 15:23:50 +0100

    Debug

--- Code/main_Version_Behrooz.py
+++ Code/main_Version_Behrooz.py
@@ -73,8 +73,13 @@ def stateMachine(ev=int):
         if state == STATE_STOP:
             theRobotController.stop()
 #-----------------------------------------------------------------------------------------------------------------------
-GPIO.add_event_detect(button_Start, GPIO.RISING, callback=stateMachine(EV_START), bouncetime=300)
-GPIO.add_event_detect(button_Stop, GPIO.RISING, callback=stateMachine(EV_STOP), bouncetime=300)
+def callbackStart(channel):
+    stateMachine(EV_START)
+def callbackStop(channel):
+    stateMachine(EV_STOP)
+
+GPIO.add_event_detect(button_Start, GPIO.RISING, callback=callbackStart, bouncetime=300)
+GPIO.add_event_detect(button_Stop, GPIO.RISING, callback=callBackStop, bouncetime=300)
 
 class RobotControl_Thread(Thread):
     def __init__(self):
